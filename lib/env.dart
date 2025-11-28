2e21a2c0c8c9dfecbb762d38b66d2bf13a109a51
Alex Peredreev <peredreev.aleksey@gmail.com>
2025-11-28 14:54:54 +0400

    add env and makefile

---
+++ lib/env.dart
@@ -0,0 +1,15 @@
+class Env {
+  static const String type = 'prod';
+
+  static const bool debug = false;
+
+  static const String baseUrl = 'https://exchats.com';
+
+  static const String proxyUrl = '';
+
+  static const int connectTimeout = 30000;
+
+  static const int receiveTimeout = 30000;
+
+  static const int sendTimeout = 30000;
+}
